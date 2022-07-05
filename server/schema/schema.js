1be5c5b33bc8bae197880a183d2429620e55096a
Cañete <raymond.j.canete@accenture.com>
2022-07-05 15:25:52 +0800

    Added Routes UI and Project Cards

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -106,7 +106,7 @@ const mutation = new GraphQLObjectType({
     deleteClient: {
       type: ClientType,
       args: {
-        id: { type: GraphQLNonNull(GraphQLString) },
+        id: { type: GraphQLNonNull(GraphQLID) },
       },
       resolve(parent, args) {
         return Client.findByIdAndRemove(args.id);
